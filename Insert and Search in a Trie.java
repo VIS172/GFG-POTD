240eb9d0e0102ff94555e697ea2508b7bd916a6e
Vismaya Satheesh <109724129+VIS172@users.noreply.github.com>
2024-01-31 18:39:54 +0530

    Add files via upload

---
+++ Insert and Search in a Trie.java
@@ -0,0 +1,24 @@
+static void insert(TrieNode root, String key) {
+    TrieNode curr=root;
+    for(int i=0;i<key.length();i++){
+        char c=key.charAt(i);
+        if(curr.children[c-'a']==null){
+            curr.children[c-'a']=new TrieNode();
+        }
+        curr=curr.children[c-'a'];
+    }
+    curr.isEndOfWord=true;
+}
+
+//Function to use TRIE data structure and search the given string.
+static boolean search(TrieNode root, String key){
+    TrieNode curr=root;
+    for(int i=0;i<key.length();i++){
+        char c=key.charAt(i);
+        if(curr.children[c-'a']==null){
+            return false;
+        }
+        curr=curr.children[c-'a'];
+    }
+    return curr.isEndOfWord;
+}
