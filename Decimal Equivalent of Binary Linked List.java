78c473428160f5c2d530fbadf7e1bae156462303
Vismaya Satheesh <109724129+VIS172@users.noreply.github.com>
2024-02-03 22:01:19 +0530

    Add files via upload

---
+++ Decimal Equivalent of Binary Linked List.java
@@ -0,0 +1,15 @@
+class Solution
+{
+   long DecimalValue(Node head)
+   {
+       long res =0;
+       int mod = 1000000007;
+       while(head!=null){
+           res = (res<<1)%mod;
+           res = (res + head.data)%mod;
+           head = head.next;
+       }
+       return res;
+     
+   }
+}
