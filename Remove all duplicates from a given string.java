13f84ff478658a02cf6f95562f4fab41f3f454d6
Vismaya Satheesh <109724129+VIS172@users.noreply.github.com>
2024-03-10 21:28:17 +0530

    Add files via upload

--- Remove all duplicates from a given string.java
+++ Remove all duplicates from a given string.java
@@ -1,15 +1,15 @@
 class Solution {
     String removeDuplicates(String str) {
-        // code here
-        StringBuilder ans = new StringBuilder();
-        HashSet<Character> st = new HashSet<>();
-        st.add(str.charAt(0));
+        HashSet<Character> hs=new HashSet<>();
+        StringBuilder ans=new StringBuilder();
+        hs.add(str.charAt(0));
         ans.append(str.charAt(0));
         for(int i=1;i<str.length();i++){
-            if(!st.contains(str.charAt(i))){
+            char ch = str.charAt(i);
+            if(!hs.contains(ch)){
                 ans.append(str.charAt(i));
+                hs.add(str.charAt(i));
             }
-            st.add(str.charAt(i));
         }
         return ans.toString();
     }
